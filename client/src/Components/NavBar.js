e2fdcf18372de054aae200216054756ee951c7e0
Demaris Ashmore <demiashmore@yahoo.com>
2023-04-04 11:37:46 -0600

    Add navbar

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -2,9 +2,9 @@ function NavBar() {
 
     return(
         <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo right">RSVPify</a>
-                <ul id="nav-mobile" class="left hide-on-med-and-down">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo right">RSVPify</a>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
                     <li><a href="sass.html">Login</a></li>
                     <li><a href="badges.html">Sign Up</a></li>
                     <li><a href="collapsible.html">JavaScript</a></li>
