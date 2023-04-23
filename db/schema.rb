7a3abf5f89a7439cb7da0fcd312cf461403f6579
Demaris Ashmore <demiashmore@yahoo.com>
2023-04-23 15:41:29 -0600

    Add user_id to guest table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_224707) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_23_212322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_224707) do
     t.boolean "family"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_guests_on_user_id"
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -59,6 +61,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_224707) do
   end
 
   add_foreign_key "events", "users"
+  add_foreign_key "guests", "users"
   add_foreign_key "invitations", "events"
   add_foreign_key "invitations", "guests"
 end
